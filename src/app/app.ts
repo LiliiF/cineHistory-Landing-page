c8f65059a23dcff7adcc69182d0166a0d414f585
Lili <liligit25@gmail.com>
2025-08-15 13:42:28 -0300

    Adiciona seção movies responsivo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { About } from './components/about/about';
 import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Movies } from './components/movies/movies';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [
-    About,
     Header,
     Hero,
+    About,
+    Movies,
     // Footer,
     RouterOutlet
   ],
