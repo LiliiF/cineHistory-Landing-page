9d413aefb337d910773b736beddbdaf7f5783762
Lili <liligit25@gmail.com>
2025-08-21 23:07:17 -0300

    Conecta itens do menu às seções

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common'; // faz o menu “pular” para cada seção
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,16 @@ import { Component } from '@angular/core';
 })
 export class Header {
   menuOpen = false;
+  activeSection: string = 'hero';
+
+  constructor(private viewportScroller: ViewportScroller) { }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  scrollToSection(section: string) {
+    this.viewportScroller.scrollToAnchor(section);
+    this.activeSection = section; // marca a seção ativa
   }
 }
